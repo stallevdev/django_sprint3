4529963204ee3d206e0bc3c9759e1c36720daad5
Sergey Tsepilov <stallevdev@gmail.com>
2024-05-05 16:30:16 +0500

    urls

---
+++ blogicum/pages/urls.py
@@ -0,0 +1,12 @@
+"""Маршруты (URL) для приложения."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('rules/', views.rules, name='rules'),
+]
